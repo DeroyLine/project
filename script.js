0e3463cf65051b4d67742750031796891b15a8fc
Kirill <bykovkirill1985@gmail.com>
2020-03-07 12:54:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let money = confirm("Ваш бюджет на месяц");
+
+ let time = confirm("Введите дату в формате YYYY-MM-DD");
+
+ let appData = {
+ 	бюджет: "money",
+ 	timeData: "time",
+ 	expenses: {},
+ 	optionalExpenses: {},
+ 	income: [],
+ 	 savings: false
+ };
+  confirm("Введите обязательную статью расходов в этом месяце");
+ confirm("Во сколько обойдется?");
+
+ewexpenses: {
+	"Paper" : "100.000rub."
+};
+ alert("5000руб");
+    
+   

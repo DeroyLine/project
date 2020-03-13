8dbcede334cf5da106ff2728f86be3fb9d486d42
Kirill <bykovkirill1985@gmail.com>
2020-03-13 19:08:57 +0300

    Homework

--- script.js
+++ script.js
@@ -1,21 +1,150 @@
-let money = confirm("Ваш бюджет на месяц");
 
- let time = confirm("Введите дату в формате YYYY-MM-DD");
+/*let money = prompt("Ваш бюджет на месяц");
+let time = prompt("Введите дату и время в формате YYYY-MM-DD");
 
- let appData = {
- 	бюджет: "money",
- 	timeData: "time",
- 	expenses: {},
- 	optionalExpenses: {},
- 	income: [],
- 	 savings: false
- };
- let vopros =  prompt("Введите обязательную статью расходов в этом месяце");
- confirm("Во сколько обойдется?");
- /*ewexpenses: {
-	когда я вожу тут информацию у меня выдает ошибку
+
+
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+for (let i = 0; i < 2; i++) {    
+	let a = prompt("Ваш бюджет на месяц");
+let b = prompt("Введите дату и время в формате YYYY-MM-DD");
+
+if (typeof(a)) === 'string' && (typeof(a)) != null &&
+ (typeof(b)) != null && a != '' && b != '' && a.length < 50{
+ 	console.log("Верно")
+ }
+
+console.log("done")
+
+appData.expenses[a] = b;
+ }
+ else {
+dafault: 
+console.log("Ошибка 0x00000000")
+}*/
+
+//let time = prompt("Впишите дату в формате YYYY-MM-DD")
+//let number = prompt("Сколько вам лет?");
+ // let money = prompt("Какая ваша зарплата?");
+
+
+
+/*
+let appData = {
+	budget: [],
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: [],
+	savings: false
 };*/
 
- alert("5000руб");
-    
-   
+
+
+//for (let i = 0; i < 3; i++) {
+
+//let a = prompt("Сколько вам лет?"),
+
+ //    b = prompt("Какая ваша зарплата?");
+
+// if ( (typeof(a)) === 'number' && (typeof(a)) != null && (typeof(a)) != '' &&
+
+ // (typeof(a)) != "string" && (typeof(b)) === "number" && (typeof(b)) != null &&
+
+  //   (typeof(b)) != ' ' && (typeof(b)) != 'string' && a.length < 3 && b.length < 300000) 
+          
+// {
+ //	console.log('done');
+
+ 	//appData.expenses[a] = b;
+
+// }
+//else {
+
+	//console.log("Извените произоошла ошибка!");
+//}
+  //};
+
+
+//alert(appData.budget / 30);
+
+ /*let school = prompt("В каком вы классе?");
+
+ let abc = prompt("Какая буква вашего класса?");
+
+  for(let all = 0; all < 2; all++)
+   {
+  	if((typeof(school)) != 'string' && (typeof(school)) != '' && school.lenght < 2 &&
+  	 (typeof(abc)) === 'string' && (typeof(abc)) != null && (typeof(abc)) != '')
+
+  	{
+  		console.log("done");
+  		appData.expenses[school] = abc;
+  	}
+  	else if(school) {
+  		alert("Вы уже не в школе"); 
+  	} // Но у меня не получилось
+  	  // Тока вроде когда ничего не пишит то и алерт и не выводится...
+  	  //Но ошибок нет
+  	    //И хотя бы код работает!
+  	else{
+  		
+  		console.log("В коду ошибка 0х00000000");
+
+  	}
+  };
+          alert(appData.budget / 30);
+*/
+//Я хотел написать типо 82381 класс то алерт выведет вы уже не в школе на пожобии ошибки если..
+
+  // Но у меня не получилось
+  	  // Тока вроде когда ничего не пишит то и алерт и не выводится...
+  	  //Но ошибок нет
+  	    //И хотя бы код работает!
+
+
+  	    /*Тренировка 
+  	      Условий....*/
+  	     /* let number = 10;*/ //let test = 10;
+/*while(number < 50) 
+{
+	console.log(number);
+
+	number++;
+
+}*/
+////////////////////////////////
+
+//for(number; number < 13; number++) {
+//	if(number == 11){
+//		break;
+//	}
+//	console.log(number);
+//}
+////////////////////////////
+
+
+//do{
+//	console.log(test);
+//	 test++;
+//	 if(test == 13){
+//	 	//break;
+//	  continue; 
+//	 }
+//}
+//while(test < 15);
+
+//let a = prompt("Как вы провели этот день?");
+// Все!

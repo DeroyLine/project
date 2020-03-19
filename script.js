523fdd4d431429bbcfeb777c956b18bbf760de32
Kirill <bykovkirill1985@gmail.com>
2020-03-19 12:48:31 +0300

    homework3

--- script.js
+++ script.js
@@ -1,150 +1,36 @@
+let money = +prompt("Бюджет на месяц", '');//50k бюджет на месяц
 
-/*let money = prompt("Ваш бюджет на месяц");
-let time = prompt("Введите дату и время в формате YYYY-MM-DD");
+let mouth = +prompt('месяц(дней)', ''); //Один месяц (в днях)
 
-
-
-
-let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
-};
-
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-
-for (let i = 0; i < 2; i++) {    
-	let a = prompt("Ваш бюджет на месяц");
-let b = prompt("Введите дату и время в формате YYYY-MM-DD");
-
-if (typeof(a)) === 'string' && (typeof(a)) != null &&
- (typeof(b)) != null && a != '' && b != '' && a.length < 50{
- 	console.log("Верно")
- }
-
-console.log("done")
-
-appData.expenses[a] = b;
- }
- else {
-dafault: 
-console.log("Ошибка 0x00000000")
-}*/
-
-//let time = prompt("Впишите дату в формате YYYY-MM-DD")
-//let number = prompt("Сколько вам лет?");
- // let money = prompt("Какая ваша зарплата?");
+ let appData = {
+ 	budget: money,
+ 	expenses: {},
+ optionalExpenses: {},
+ income: [],
+ timeData: [],
+ savings: true
+ };
+ function start() {
+ money = +prompt("Бюджет на месяц", '');
+   mouth = +prompt('месяц(дней)', '');
+      while(isNaN(money) || money == '' || money == null) {
+   	     money = +prompt("Бюджет на месяц", '');
+       }
+ } 
+ start();
 
 
 
-/*
-let appData = {
-	budget: [],
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: [],
-	savings: false
-};*/
+function result() {
+	if(appData.savings == true) {
+		let money = +prompt("Бюджет на месяц", '');
+		let mouth = +prompt('месяц(дней)', '');
+		appData.mouthIncome = money/mouth;
 
-
-
-//for (let i = 0; i < 3; i++) {
-
-//let a = prompt("Сколько вам лет?"),
-
- //    b = prompt("Какая ваша зарплата?");
-
-// if ( (typeof(a)) === 'number' && (typeof(a)) != null && (typeof(a)) != '' &&
-
- // (typeof(a)) != "string" && (typeof(b)) === "number" && (typeof(b)) != null &&
-
-  //   (typeof(b)) != ' ' && (typeof(b)) != 'string' && a.length < 3 && b.length < 300000) 
-          
-// {
- //	console.log('done');
-
- 	//appData.expenses[a] = b;
-
-// }
-//else {
-
-	//console.log("Извените произоошла ошибка!");
-//}
-  //};
-
-
-//alert(appData.budget / 30);
-
- /*let school = prompt("В каком вы классе?");
-
- let abc = prompt("Какая буква вашего класса?");
-
-  for(let all = 0; all < 2; all++)
-   {
-  	if((typeof(school)) != 'string' && (typeof(school)) != '' && school.lenght < 2 &&
-  	 (typeof(abc)) === 'string' && (typeof(abc)) != null && (typeof(abc)) != '')
-
-  	{
-  		console.log("done");
-  		appData.expenses[school] = abc;
-  	}
-  	else if(school) {
-  		alert("Вы уже не в школе"); 
-  	} // Но у меня не получилось
-  	  // Тока вроде когда ничего не пишит то и алерт и не выводится...
-  	  //Но ошибок нет
-  	    //И хотя бы код работает!
-  	else{
-  		
-  		console.log("В коду ошибка 0х00000000");
-
-  	}
-  };
-          alert(appData.budget / 30);
-*/
-//Я хотел написать типо 82381 класс то алерт выведет вы уже не в школе на пожобии ошибки если..
-
-  // Но у меня не получилось
-  	  // Тока вроде когда ничего не пишит то и алерт и не выводится...
-  	  //Но ошибок нет
-  	    //И хотя бы код работает!
-
-
-  	    /*Тренировка 
-  	      Условий....*/
-  	     /* let number = 10;*/ //let test = 10;
-/*while(number < 50) 
-{
-	console.log(number);
-
-	number++;
-
-}*/
-////////////////////////////////
-
-//for(number; number < 13; number++) {
-//	if(number == 11){
-//		break;
-//	}
-//	console.log(number);
-//}
-////////////////////////////
-
-
-//do{
-//	console.log(test);
-//	 test++;
-//	 if(test == 13){
-//	 	//break;
-//	  continue; 
-//	 }
-//}
-//while(test < 15);
-
-//let a = prompt("Как вы провели этот день?");
-// Все!
+		alert("Ваш доход на один день составляет: " + appData.mouthIncome);
+	}
+}
+result();
+//работает..
+   //Все работает, только почему все хорошо, но вся функция в страничке самой по три раза идет..
+   // Может потому-что у меня остального кода нету
